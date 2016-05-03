442be5fab2716b9ee513892fcf610268b42552f8
Simon Fischer <mail@simonfischer.com>
2016-05-03 12:07:33 +0200

    njah

--- public/scripts/chatFunctions.js
+++ public/scripts/chatFunctions.js
@@ -1,6 +1,7 @@
 var socket = io.connect('http://localhost:5000');
 
 socket.emit('chat message', { my: 'data' });
+console.log("send data");
 socket.on('chat message', function (data) {
     console.log(data);
 });
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,47 @@ var router = express.Router();
 var http = require('http').Server(router);
 var io = require('socket.io')(http);
 
+
+var middleware = require('socketio-wildcard')();
+
+io.use(middleware);
+
+io.on('connection', function(socket) {
+  socket.on('*', function(){ 
+    console.log("recived socketio-wildcard")
+  });
+});
+
+io.listen(5000);
+
+/*
+var globalEvent = "*";
+io.$emit = function (name) {
+    if(!this.$events) return false;
+    for(var i=0;i<2;++i){
+        if(i==0 && name==globalEvent) continue;
+        var args = Array.prototype.slice.call(arguments, 1-i);
+        var handler = this.$events[i==0?name:globalEvent];
+        if(!handler) handler = [];
+        if ('function' == typeof handler) handler.apply(this, args);
+        else if (io.util.isArray(handler)) {
+            var listeners = handler.slice();
+            for (var i=0, l=listeners.length; i<l; i++)
+                listeners[i].apply(this, args);
+        } else return false;
+    }
+    return true;
+};
+
 io.on('connection', function(socket){
   console.log("connection");
-  socket.on('chat message', function(msg){
+  socket.on(globalEvent, function(msg){
+    console.log("got a *")
     io.emit('chat message', msg);
   });
 });
 
-http.listen(5000, function(){});
+http.listen(5000, function(){});*/
 
 
 function handleBaseDesign(req, res, next){
